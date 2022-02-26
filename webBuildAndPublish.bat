b668328e90e94644a7f7a110585ced8dc27b449c
xEyad <eyiadhisham@yahoo.com>
2022-02-26 19:16:26 +0200

    slightly modified bat file

--- webBuildAndPublish.bat
+++ webBuildAndPublish.bat
@@ -1,20 +1,20 @@
 @ECHO OFF
-@REM ECHO Building project for web and publishing to git
-@REM ECHO flutter build web --release --base-href /algo_visualizer/ 
-@REM call flutter build web --release --base-href /algo_visualizer/ 
+ECHO Building project for web and publishing to git
+ECHO flutter build web --release --base-href /algo_visualizer/ 
+call flutter build web --release --base-href /algo_visualizer/ 
 
 ECHO copy contents of build/web to docs/
-call xcopy "build/web" "docs/" /y
+call xcopy "build/web" "docs" /y /i
 
 ECHO duplicate your index.html and rename it to 404.html
-call xcopy "docs/index.html" "docs/404.html" /y
+call xcopy "docs\index.html" "docs\404.html" /Y
 
 ECHO git add and commit all with message "new web version"
-git add .
-git commit -m "new web version"
+call git add .
+call git commit -m "new web version"
 
-ECHO git push
-git push
+@REM ECHO git push
+@REM call git push
 
-ECHO Congrats, everything is ready. updates should appear on this link https://xeyad.github.io/algo_visualizer within few short minutes 
-PAUSE
+@REM ECHO Congrats, everything is ready. updates should appear on this link https://xeyad.github.io/algo_visualizer within few short minutes 
+@REM PAUSE
